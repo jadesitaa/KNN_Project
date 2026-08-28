cc06a89fe67f2db283bc5b553727e4450a6b12d7
jadesitaa <jadesita.work@gmail.com>
2026-08-28 05:28:22 +0000

    feat: transform numerical data

--- Titanicmodel_compare.py
+++ Titanicmodel_compare.py
@@ -1,2 +1,9 @@
+
 import pandas as pd
 df = pd.read_csv('/content/Titanic-Dataset.csv')
+
+# เช็คแล้วไม่มีค่าว่าง
+print(df.isnull().sum())
+#แปลง column ที่ไม่ใช่ตัวเลข
+print(df.dtypes)
+df = pd.get_dummies(df, columns=['Sex', 'Embarked'], drop_first=True)
