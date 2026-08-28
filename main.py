08fe9e163d2f2dd84962e6bb00d359ea4e9a30f1
jadesitaa <jadesita.work@gmail.com>
2026-08-28 01:41:44 +0000

    feat: implement decision tree model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
 
 data = {
     'Color': ['Red', 'Red', 'Red', 'Yellow', 'Yellow', 'Yellow', 'Yellow', 'Yellow', 'Red', 'Red'],
@@ -7,4 +9,10 @@ data = {
     'Stolen': ['Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'Yes']
 }
 df = pd.DataFrame(data)
-print("Dataset loaded successfully")
+X = pd.get_dummies(df[['Color', 'Type', 'Origin']])
+y = df['Stolen']
+
+# สร้างและเทรนโมเดล
+clf = DecisionTreeClassifier(criterion='entropy', random_state=42)
+clf.fit(X, y)
+print("Model trained successfully")
