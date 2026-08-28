7ce795166139d6f39d0848af2e1dd41e89a695a3
jadesitaa <jadesita.work@gmail.com>
2026-08-28 01:41:01 +0000

    feat: setup dataset

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-
-import matplotlib.pyplot as plt
+data = {
+    'Color': ['Red', 'Red', 'Red', 'Yellow', 'Yellow', 'Yellow', 'Yellow', 'Yellow', 'Red', 'Red'],
+    'Type': ['Sport', 'Sport', 'Sport', 'Sport', 'Sport', 'SUV', 'SUV', 'SUV', 'SUV', 'Sport'],
+    'Origin': ['Domestic', 'Domestic', 'Domestic', 'Domestic', 'Imported', 'Imported', 'Imported', 'Domestic', 'Imported', 'Imported'],
+    'Stolen': ['Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'Yes']
+}
+df = pd.DataFrame(data)
+print("Dataset loaded successfully")
